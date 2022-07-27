7fd5707fbf6d86cbfcd3494b58156a0aaec0065a
YAY2011 <88600273+YAY2011@users.noreply.github.com>
2022-07-27 19:40:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+https://teachablemachine.withgoogle.com/models/1zcT6a9mD/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classify = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1zcT6a9mD/model.json',modelLoaded);
+function modelLoaded() {
+    console.log('Model Loaded! :D');
+}
